e0c4d4397a0f3f4ebba62319ce3bcfa43bc8992a
unknown <alexisreyes90@gmail.com>
2018-12-13 00:38:59 -0500

    re-did register/ login/ reducer functionality

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,9 +1,42 @@
 import axios from 'axios';
 
 export const LOADING = 'LOADING';
-export const SUCCESS = 'SUCCESS';
-export const ERROR = 'ERROR';
+export const REGISTER = 'REGISTER';
+export const LOGIN = 'LOGIN';
+export const CHANGE_LOADING = 'CHANGE_LOADING';
 
-/*
-Have team decide what our api should be to use axios calls
- */
+export const registerUser = (user) => {
+    return dispatch => {
+        dispatch({ type: LOADING });
+        axios.post('/register', user)
+            .then(response => {
+                dispatch({
+                    type: REGISTER,
+                    payload: response.data
+                })
+                dispatch({ type: CHANGE_LOADING });
+            })
+            .catch(err => {
+                dispatch({ type: CHANGE_LOADING });
+                console.log(err)
+            })
+    }
+}
+
+
+export const loginUser = () => {
+    return dispatch => {
+        dispatch({ type: LOADING })
+        axios.get('/login')
+            .then(response => {
+                // response will carry token/ encryption from backend
+                dispatch({
+                    type: LOGIN
+                })
+            })
+            .catch(err => {
+                dispatch({ type: CHANGE_LOADING });
+                console.log(err)
+            })
+    }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import rootReducer from './reducers';
-
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import rootReducer from './reducers';
+import Register from './components/register';
+import Login from './components/login';
+import App from './components/App.js';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // subscribe app to reducer/ store
 const store = createStore(
     rootReducer,
-    applyMiddleware(thunk, logger)
+    composeEnhancer(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router>
+            <div className="page-container">
+                <Route exact path="/" component={App} />
+                <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
+            </div>
+        </Router>
     </Provider>
 , document.getElementById('root'));
 
