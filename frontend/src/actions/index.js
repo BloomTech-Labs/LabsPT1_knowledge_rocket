77afed7892c51036c7573e6e74daad42d651839c
Bhavik Ravani <ravanibhavik@gmail.com>
2018-12-15 14:35:11 -0500

    Updated endpoints for login and register.

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -7,3 +7,38 @@ export const ERROR = 'ERROR';
 /*
 Have team decide what our api should be to use axios calls
  */
+export const registerUser = (user) => {
+    return dispatch => {
+        dispatch({ type: LOADING });
+        axios.post('http://localhost:8000/register/', user)
+            .then(response => {
+                dispatch({
+                    type: REGISTER,
+                    payload: response.data
+                })
+                dispatch({ type: CHANGE_LOADING });
+            })
+            .catch(err => {
+                dispatch({ type: CHANGE_LOADING });
+                console.log(err)
+            })
+    }
+}
+
+
+export const loginUser = (user) => {
+    return dispatch => {
+        dispatch({ type: LOADING })
+        axios.post('http://localhost:8000/login/', user)
+            .then(response => {
+                // response will carry token/ encryption from backend
+                dispatch({
+                    type: LOGIN
+                })
+            })
+            .catch(err => {
+                dispatch({ type: CHANGE_LOADING });
+                console.log(err)
+            })
+    }
+}
