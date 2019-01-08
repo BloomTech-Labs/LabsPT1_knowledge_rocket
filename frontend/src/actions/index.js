c4f0a685776a85e66acca4a7704bbb26810a3696
Lisa Canini <three_nineteen@yahoo.com>
2019-01-07 21:23:28 -0800

    new server endpoint

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -6,38 +6,35 @@ export const LOGIN = 'LOGIN';
 export const CHANGE_LOADING = 'CHANGE_LOADING';
 
 //heroku: https://knowledgerocketjr.herokuapp.com/
+// https://cspt1knowledgerocket.herokuapp.com/ ** group deploy
 
-export const registerUser = (user) => {
-    return dispatch => {
-        dispatch({ type: LOADING });
-        axios.post('https://nameless-beyond-28288.herokuapp.com/register/', user)
-            .then(response => {
-                dispatch({
-                    type: REGISTER,
-                    payload: response.data
-                })
-                // dispatch({ type: CHANGE_LOADING });
-            })
-            .catch(err => {
-                dispatch({ type: CHANGE_LOADING });
-                console.log(err)
-            })
-    }
-}
+https: export const registerUser = user => {
+  return dispatch => {
+    dispatch({ type: LOADING });
+    axios
+      .post("https://cspt1knowledgerocket.herokuapp.com/register/", user)
+      .then(response => {
+        dispatch({ type: REGISTER, payload: response.data });
+        // dispatch({ type: CHANGE_LOADING });
+      })
+      .catch(err => {
+        dispatch({ type: CHANGE_LOADING });
+        console.log(err);
+      });
+  };
+};
 
 export const loginUser = (user) => {
     return dispatch => {
         dispatch({ type: LOADING })
-        axios.post('https://knowledgerocketjr.herokuapp.com/login/', user)
-            .then(response => {
-                dispatch({
-                    type: LOGIN,
-                    payload: response.data
-                })
-            })
-            .catch(err => {
-                dispatch({ type: CHANGE_LOADING });
-                console.log(err)
-            })
+        axios
+          .post("https://cspt1knowledgerocket.herokuapp.com/login/", user)
+          .then(response => {
+            dispatch({ type: LOGIN, payload: response.data });
+          })
+          .catch(err => {
+            dispatch({ type: CHANGE_LOADING });
+            console.log(err);
+          });
     }
 }
