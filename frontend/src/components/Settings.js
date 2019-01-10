a1200ae52f81dd0e0fb721528fae1879ba346c18
Lisa Canini <three_nineteen@yahoo.com>
2019-01-09 20:49:24 -0800

    super ugly wip

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -5,21 +5,23 @@ import { Button, Form, Label, Input } from "reactstrap";
 import { Container } from "reactstrap";
 
 import SidebarNav from "./SidebarNav";
+import "../css/SidebarNav.css"
 import "../css/Settings.css";
 
 const Settings = () => {
   return (
     <div>
       <SidebarNav />
-      <div>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <a href="/">Home</a>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>Settings</BreadcrumbItem>
-        </Breadcrumb>
-      </div>
+      
       <div className="main">
+        <div>
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <a href="/">Home</a>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>Settings</BreadcrumbItem>
+          </Breadcrumb>
+        </div>
         <Container>
           <div id="setting-form">
             <Form>
