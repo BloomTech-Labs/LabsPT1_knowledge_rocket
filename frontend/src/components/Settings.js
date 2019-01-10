e613f710f8f46b132b9e18c663d85bc3b0956c18
Lisa Canini <three_nineteen@yahoo.com>
2019-01-10 14:51:40 -0800

    single form group to tidy code

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -16,11 +16,12 @@ class Settings extends Component {
     password2: ""
   };
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
-    console.log(this.state)
-    return( <Container className="container">
+    console.log(this.state);
+    return (
+      <Container className="container">
         <Row>
           <Col sm="md" lg="3">
             <SidebarNav />
@@ -28,42 +29,68 @@ class Settings extends Component {
           <Col>
             <Row>
               <Col>
-              <div>
-                <Breadcrumb>
-                  <BreadcrumbItem>
-                    <a href="/">Home</a>
-                  </BreadcrumbItem>
-                  <BreadcrumbItem active>Settings</BreadcrumbItem>
-                </Breadcrumb>
-              </div>
+                <div>
+                  <Breadcrumb>
+                    <BreadcrumbItem>
+                      <a href="/">Home</a>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>Settings</BreadcrumbItem>
+                  </Breadcrumb>
+                </div>
               </Col>
             </Row>
             <Row>
               <Col>
-              <Form>
-                <FormGroup>
-                  <Label for="exampleEmail">Email</Label>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="email" value={ this.state.email } onChange={ this.handleInputChange } />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="examplePassword">Current Password</Label>
-                  <Input type="password" name="password" id="examplePassword" placeholder="password" value={ this.state.password } onChange={ this.handleInputChange } />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="examplePassword">New Password</Label>
-                  <Input type="password" name="password1" id="examplePassword" placeholder="password" value={ this.state.password1 } onChange={ this.handleInputChange } />
-                </FormGroup>
-              <FormGroup>
-                <Label for="examplePassword">Confirm Password</Label>
-                  <Input type="password" name="password2" id="examplePassword" placeholder="password" value={ this.state.password2 } onChange={ this.handleInputChange } />
-              </FormGroup>
-              <Button>Update</Button>
-              </Form>
-            </Col>
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="exampleEmail"
+                      placeholder="email"
+                      value={this.state.email}
+                      onChange={this.handleInputChange}
+                    />
+
+                    <Label for="examplePassword">Current Password</Label>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="examplePassword"
+                      placeholder="password"
+                      value={this.state.password}
+                      onChange={this.handleInputChange}
+                    />
+
+                    <Label for="examplePassword">New Password</Label>
+                    <Input
+                      type="password"
+                      name="password1"
+                      id="examplePassword"
+                      placeholder="password"
+                      value={this.state.password1}
+                      onChange={this.handleInputChange}
+                    />
+
+                    <Label for="examplePassword">Confirm Password</Label>
+                    <Input
+                      type="password"
+                      name="password2"
+                      id="examplePassword"
+                      placeholder="password"
+                      value={this.state.password2}
+                      onChange={this.handleInputChange}
+                    />
+                  </FormGroup>
+                  <Button>Update</Button>
+                </Form>
+              </Col>
             </Row>
           </Col>
         </Row>
-      </Container>)
+      </Container>
+    );
   }
 }
 
