2a7e50dde4bdcc2df7fe66230d7da3d596487b56
Lisa Canini <three_nineteen@yahoo.com>
2019-01-10 19:37:37 -0800

    resubmit pr for setting page

--- Readme.md
+++ Readme.md
@@ -44,13 +44,23 @@ Knowledge rockets are short paragraphs followed by a multiple choice question. T
 - Heroku
 
 ## Front End Host:
-- To Be Determined
+- Netlify
 
 ## Billing:
 - Stripe
 
 ## Notifications:
 - Sendgrid
+    proper syntax for sending email via python shell:
+        from django.core.mail import send_mail
+        send_mail(
+            'First Email',
+            'test!',
+            'jesse.reichel@live.com',
+            ['cspt1knowledgerocket@gmail.com'],
+            fail_silently=False,
+        )
+
 
 ## Auth:
 - JWT
@@ -62,7 +72,6 @@ Knowledge rockets are short paragraphs followed by a multiple choice question. T
 - ES Lint
 
 ## Team Members:
-- [Stephanie Bowles](https://github.com/sbowl001) --+(UTC + 8 hours)[to be determined...]
 - [Lisa Canini](https://github.com/LisaCee)--Pacific Time Zone
 - [Bhavik Ravani](https://github.com/ravb2019)--Eastern Time Zone
 - [Jesse Reichel](https://github.com/KryoKorpz)--Pacific Time Zone
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import * as serviceWorker from './serviceWorker';
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import rootReducer from './reducers';
-import Register from './components/register';
-import Login from './components/login';
-import App from './components/App.js';
-import Classes from './components/Classes.js';
-import Settings from './components/Settings.js';
+import rootReducer from "./reducers";
+import Register from "./components/register";
+import Login from "./components/login";
+import App from "./components/App.js";
+import Classes from "./components/Classes.js";
+import Settings from "./components/Settings.js";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // subscribe app to reducer/ store
 const store = createStore(
-    rootReducer,
-    composeEnhancer(applyMiddleware(thunk, logger))
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <div className="page-container">
-                <Route exact path="/" component={App} />
-                <Route path="/register" component={Register} />
-                <Route path="/login" component={Login} />
-                <Route path="/classes" component={Classes} />
-                <Route path="/settings" component={Settings} />
-            </div>
-        </Router>
-    </Provider>
-, document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <div className="page-container">
+        <Route exact path="/" component={App} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/classes" component={Classes} />
+        <Route path="/settings" component={Settings} />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // keep as unregistered to work off of a port
 serviceWorker.unregister();
