34da439dd0cf5975f3f71749386c313b595927df
Lisa Canini <three_nineteen@yahoo.com>
2019-01-12 11:06:26 -0800

    redid layout

--- frontend/src/components/Classes.js
+++ frontend/src/components/Classes.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import '../css/Classes.css';
+import React, { Component } from "react";
+import "../css/Classes.css";
 
 import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
-import SidebarNav from './SidebarNav.js';
+import SidebarNav from "./SidebarNav.js";
 
-const Classes = (props) => {
-    return(
-        <div>
-            <SidebarNav />
-            <div>
-                <Breadcrumb>
-                    <BreadcrumbItem><a href="/">Home</a></BreadcrumbItem>
-                    <BreadcrumbItem active>Classes</BreadcrumbItem>
-                </Breadcrumb>
-            </div>
-            <div className='main'></div>
-        </div>
-    )
+class Classes extends Component {
+  render() {
+    return (
+        <Container className="container">
+            <Row>
+                <Col sm="md" lg="3">
+                    <SidebarNav />
+                </Col>
+                <Col>
+                    <Row>
+                        <Col>
+                            <div>
+                                <Breadcrumb>
+                                    <BreadcrumbItem>
+                                        <a href="/">Home</a>
+                                    </BreadcrumbItem>
+                                    <BreadcrumbItem active>Settings</BreadcrumbItem>
+                                </Breadcrumb>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                           
+                        </Col>
+                    </Row>
+                </Col>
+                </Row>
+        </Container>
+    );
+  }
 }
 
 export default Classes;
