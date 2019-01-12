539d13a0eaf9a3bec7da8111f4d24a276caef2d5
unknown <jesse.reichel@live.com>
2019-01-12 11:27:14 -0800

    registered admin views

--- rocketmaster/settings.py
+++ rocketmaster/settings.py
@@ -161,4 +161,4 @@ STATIC_URL = '/static/'
 
 import django_heroku
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
