ae784384b5389c453d5c4c717ba5efe61b1dd8c7
darthMinimus <irishseanvaldivia@gmail.com>
2019-01-14 10:31:45 -0600

    Full skeleton of CreateRocket.js file.

--- frontend/src/components/CreateRocket.js
+++ frontend/src/components/CreateRocket.js
@@ -3,6 +3,33 @@ import React from 'react';
 class CreateRocket extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.state = {
+      twoDays: {
+        text: '',
+        question: '',
+        answer1: '',
+        answer2: '',
+        answer3: '',
+        answer4: ''
+      },
+      twoWeeks: {
+        text: '',
+        question: '',
+        answer1: '',
+        answer2: '',
+        answer3: '',
+        answer4: ''
+      },
+      twoMonths: {
+        text: '',
+        question: '',
+        answer1: '',
+        answer2: '',
+        answer3: '',
+        answer4: ''
+      }
+    }
   }
 
   render() {
@@ -24,22 +51,94 @@ class CreateRocket extends React.Component {
           </label>
           <label>
             <div>
-              Question 1
+              Answer 1
               <textarea/>
               <input type="radio" />{' '}
             </div>
             <div>
-              Question 2
+              Answer 2
               <textarea/>
               <input type="radio" />{' '}
             </div>
             <div>
-              Question 3
+              Answer 3
               <textarea/>
               <input type="radio" />{' '}
             </div>
             <div>
-              Question 4
+              Answer 4
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+          </label>
+        </form>
+        <form>
+          <label>
+            Name
+            <input />
+          </label>
+          <label>
+            Two Days - Review Text
+            <textarea />
+          </label>
+          <label>
+            Two Days - Question
+            <textarea />
+          </label>
+          <label>
+            <div>
+              Answer 1
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+            <div>
+              Answer 2
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+            <div>
+              Answer 3
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+            <div>
+              Answer 4
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+          </label>
+        </form>
+        <form>
+          <label>
+            Name
+            <input />
+          </label>
+          <label>
+            Two Days - Review Text
+            <textarea />
+          </label>
+          <label>
+            Two Days - Question
+            <textarea />
+          </label>
+          <label>
+            <div>
+              Answer 1
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+            <div>
+              Answer 2
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+            <div>
+              Answer 3
+              <textarea/>
+              <input type="radio" />{' '}
+            </div>
+            <div>
+              Answer 4
               <textarea/>
               <input type="radio" />{' '}
             </div>
