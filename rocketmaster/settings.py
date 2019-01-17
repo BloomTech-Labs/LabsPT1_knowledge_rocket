344926011df331617ed78c9bb7fac473e6740974
ravb2019 <ravanibhavik@gmail.com>
2019-01-17 18:32:20 -0500

    Revert settings.py

--- rocketmaster/settings.py
+++ rocketmaster/settings.py
@@ -174,4 +174,4 @@ STATIC_URL = '/static/'
 import django_heroku
 django_heroku.settings(locals())
 
-# del DATABASES['default']['OPTIONS']['sslmode']
+del DATABASES['default']['OPTIONS']['sslmode']
