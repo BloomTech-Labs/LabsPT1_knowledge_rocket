97ee5c4ef5c46529ad57177b4600189be7deab97
unknown <jesse.reichel@live.com>
2019-01-21 20:36:10 -0800

    front end, corrected indentations, added links, added fields to rocket

--- frontend/src/components/CreateRocket.js
+++ frontend/src/components/CreateRocket.js
@@ -11,6 +11,7 @@ class CreateRocket extends Component {
   state = {
     name: '',
 
+    day2QuestionName: '',
     day2ReviewText: '',
     day2Question: '',
     day2Answer1: '',
@@ -19,7 +20,7 @@ class CreateRocket extends Component {
     day2Answer4: '',
     day2SelectedAnswer: '',
 
-
+    week2QuestionName: '',
     week2ReviewText: '',
     week2Question: '',
     week2Answer1: '',
@@ -28,6 +29,7 @@ class CreateRocket extends Component {
     week2Answer4: '',
     week2SelecedAnswer: '',
 
+    month2QuestionName: '',
     month2ReviewText: '',
     month2Question: '',
     month2Answer1: '',
@@ -43,9 +45,13 @@ class CreateRocket extends Component {
   }
 
   handleRadioSelect = e => {
-    // console.log(e.target.id);
     this.setState({ [e.target.name]: this.state[e.target.id] })
     console.log(this.state);
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
   }
 
   render() {
@@ -65,8 +71,19 @@ class CreateRocket extends Component {
                       type="text"
                       name="name"
                       id="name"
+                      maxLength="95"
                       placeholder="Enter Rocket Name"
                       value={this.state.name}
+                      onChange={this.handleInputChange}
+                    />
+                    <Label for="day2QuestionName">Two Days - Question Name</Label>
+                    <Input
+                      type="text"
+                      name="day2QuestionName"
+                      id="d2QuestionName"
+                      maxLength="95"
+                      placeholder="Enter Name for Question:"
+                      value={this.state.day2QuestionName}
                       onChange={this.handleInputChange}
                     />
                     <Label for="day2ReviewText">Two Days - Review Text</Label>
@@ -74,6 +91,7 @@ class CreateRocket extends Component {
                       type="text"
                       name="day2ReviewText"
                       id="d2ReviewText"
+                      maxLength="505"
                       placeholder="Enter Day 2 Review Text: "
                       value={this.state.day2ReviewText}
                       onChange={this.handleInputChange}
@@ -83,11 +101,13 @@ class CreateRocket extends Component {
                       type="text"
                       name="day2Question"
                       id="d2Question"
+                      maxLength="505"
                       placeholder="Enter Day 2 Question: "
                       value={this.state.day2Question}
                       onChange={this.handleInputChange}
                     />
                   </FormGroup>
+                  <div> Select Radio button for correct answer, limit 1 per question</div>
                   <FormGroup className="answerChoices">
                     <div className="answer">
                       <Input type="radio"
@@ -101,77 +121,97 @@ class CreateRocket extends Component {
                           type="text"
                           name="day2Answer1"
                           id="d2answer1"
+                          maxLength="45"
                           value={this.state.day2Answer1}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                      <Input type="radio"
+                      <Input 
+                        type="radio"
                         id="day2Answer2"
                         name="day2SelectedAnswer"
                         value={this.state.day2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a2">Answer 2
-                    <Input
+                        <Input
                           type="text"
                           name="day2Answer2"
                           id="d2answer2"
+                          maxLength="45"
                           value={this.state.day2Answer2}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                      <Input type="radio"
-                        id="day2Answer3"
+                      <Input 
+                        type="radio"
                         name="day2SelectedAnswer"
+                        id="day2Answer3"
                         value={this.state.day2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a3">Answer 3
-                    <Input
+                        <Input
                           type="text"
                           name="day2Answer3"
                           id="d2answer3"
+                          maxLength="45"
                           value={this.state.day2Answer3}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                      <Input type="radio"
-                        id="day2Answer4"
+                      <Input 
+                        type="radio"
                         name="day2SelectedAnswer"
+                        id="day2Answer4"
                         value={this.state.day2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a4">Answer 4
-                    <Input
+                       <Input
                           type="text"
                           name="day2Answer4"
                           id="d2answer4"
+                          maxLength="45"
                           value={this.state.day2Answer4}
                           onChange={this.handleInputChange}
-                        /></Label>
+                        />
+                      </Label>
                     </div>
                   </FormGroup>
                   <FormGroup>
-                    <Label for="week2ReviewText">Two Weeks - Review Text</Label>
+                  <Label for="week2QuestionName">Two Weeks - Question Name</Label>
+                  <Input
+                    type="text"
+                    name="week2QuestionName"
+                    id="w2QuestionName"
+                    maxLength="95"
+                    placeholder="Enter Name for Question:"
+                    value={this.state.week2QuestionName}
+                    onChange={this.handleInputChange}
+                  />
+                  <Label for="week2ReviewText">Two Weeks - Review Text</Label>
                     <Input
                       type="text"
                       name="week2ReviewText"
                       id="w2ReviewText"
+                      maxLength="505"
                       placeholder="Enter Week 2 Review Text: "
                       value={this.state.week2ReviewText}
                       onChange={this.handleInputChange}
                     />
-                    <Label for="week2Question">Two Weeks - Question</Label>
+                  <Label for="week2Question">Two Weeks - Question</Label>
                     <Input
                       type="text"
                       name="week2Question"
                       id="w2Question"
+                      maxLength="505"
                       placeholder="Enter Week 2 Question: "
                       value={this.state.week2Question}
                       onChange={this.handleInputChange}
@@ -179,7 +219,8 @@ class CreateRocket extends Component {
                   </FormGroup>
                   <FormGroup className="answerChoices">
                     <div className="answer">
-                    <Input type="radio"
+                      <Input 
+                        type="radio"
                         id="week2Answer1"
                         name="week2SelectedAnswer"
                         value={this.state.week2SelectedAnswer}
@@ -190,68 +231,84 @@ class CreateRocket extends Component {
                           type="text"
                           name="week2Answer1"
                           id="w2answer1"
+                          maxLength="45"
                           value={this.state.week2Answer1}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="week2Answer2"
                         name="week2SelectedAnswer"
                         value={this.state.week2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a2">Answer 2
-                    <Input
+                        <Input
                           type="text"
                           name="week2Answer2"
                           id="w2answer2"
+                          maxLength="45"
                           value={this.state.week2Answer2}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="week2Answer3"
                         name="week2SelectedAnswer"
                         value={this.state.week2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a3">Answer 3
-                    <Input
+                        <Input
                           type="text"
                           name="week2Answer3"
                           id="w2answer3"
+                          maxLength="45"
                           value={this.state.week2Answer3}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="week2Answer4"
                         name="week2SelectedAnswer"
                         value={this.state.week2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a4">Answer 4
-                    <Input
+                        <Input
                           type="text"
                           name="week2Answer4"
                           id="w2answer4"
+                          maxLength="45"
                           value={this.state.week2Answer4}
                           onChange={this.handleInputChange}
-                        /></Label>
+                        />
+                      </Label>
                     </div>
                   </FormGroup>
                   <FormGroup>
+                    <Label for="month2QuestionName">Two Months - Question Name</Label>
+                    <Input
+                      type="text"
+                      name="month2QuestionName"
+                      id="m2QuestionName"
+                      maxLength="95"
+                      placeholder="Enter Name for Question:"
+                      value={this.state.month2QuestionName}
+                      onChange={this.handleInputChange}
+                    />
                     <Label for="month2ReviewText">Two Months - Review Text</Label>
                     <Input
                       type="text"
                       name="month2ReviewText"
                       id="m2ReviewText"
+                      maxLength="505"
                       placeholder="Enter Month 2 Review Text: "
                       value={this.state.month2ReviewText}
                       onChange={this.handleInputChange}
@@ -261,6 +318,7 @@ class CreateRocket extends Component {
                       type="text"
                       name="month2Question"
                       id="m2Question"
+                      maxLength="505"
                       placeholder="Enter Month 2 Question: "
                       value={this.state.month2Question}
                       onChange={this.handleInputChange}
@@ -268,7 +326,7 @@ class CreateRocket extends Component {
                   </FormGroup>
                   <FormGroup className="answerChoices">
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="month2Answer1"
                         name="month2SelectedAnswer"
                         value={this.state.month2SelectedAnswer}
@@ -279,63 +337,69 @@ class CreateRocket extends Component {
                           type="text"
                           name="month2Answer1"
                           id="m2answer1"
+                          maxLength="45"
                           value={this.state.month2Answer1}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="month2Answer2"
                         name="month2SelectedAnswer"
                         value={this.state.month2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a2">Answer 2
-                    <Input
+                        <Input
                           type="text"
                           name="month2Answer2"
                           id="m2answer2"
+                          maxLength="45"
                           value={this.state.month2Answer2}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="month2Answer3"
                         name="month2SelectedAnswer"
                         value={this.state.month2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a3">Answer 3
-                    <Input
+                        <Input
                           type="text"
                           name="month2Answer3"
                           id="m2answer3"
+                          maxLength="45"
                           value={this.state.month2Answer3}
                           onChange={this.handleInputChange}
                         />
                       </Label>
                     </div>
                     <div className="answer">
-                    <Input type="radio"
+                      <Input type="radio"
                         id="month2Answer4"
                         name="month2SelectedAnswer"
                         value={this.state.month2SelectedAnswer}
                         onClick={this.handleRadioSelect}
                       />
                       <Label for="a4">Answer 4
-                    <Input
+                        <Input
                           type="text"
                           name="month2Answer4"
                           id="m2answer4"
+                          maxLength="45"
                           value={this.state.day2Answer4}
                           onChange={this.handleInputChange}
                         /></Label>
                     </div>
+                    <Button color="info" onClick={this.handleSubmit}>
+                    Create Rocket!
+                    </Button>
                   </FormGroup>
-                  <Button>Submit</Button>
                 </Form>
               </Col>
             </Row>
