40147c48d3a0bdfc4692f501dd53797ee26b65ed
unknown <jesse.reichel@live.com>
2019-01-22 20:10:33 -0800

    error checking enabled

--- rocketsapp/api.py
+++ rocketsapp/api.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers, viewsets, generics, permissions, status
+from django.db import IntegrityError
 from django.http import JsonResponse
 from .models import Rocket, Class, Question2D, Question2M, Question2W ##, Choice, Student
 from django.contrib.auth.models import User
@@ -98,68 +99,85 @@ class RegisterRockets(generics.CreateAPIView):
         month2CorrectAnswer = request.data.get("month2CorrectAnswer")
 
         classKey = Class.objects.get(name = className) #Searches class table to find matching class name then sets it to variable, which is then applied to Rocket.save()
-    
-        Rocket(
-            rocketName = rocketName, 
-            classKey = classKey, 
-            user = username, 
-        ).save()
-        
-        rocket = Rocket.objects.get(rocketName = rocketName)
-
-        Question2D(
-            rocket = rocket,
-            day2QuestionName = day2QuestionName,
-            day2ReviewText = day2ReviewText,
-            day2QuestionText = day2QuestionText,
-            day2AnswerA = day2AnswerA,
-            day2AnswerB = day2AnswerB,
-            day2AnswerC = day2AnswerC,
-            day2AnswerD = day2AnswerD,
-            day2CorrectAnswer = day2CorrectAnswer
-        ).save()
-
-        Question2W(
-            rocket = rocket,
-            week2QuestionName = week2QuestionName,
-            week2ReviewText = week2ReviewText,
-            week2QuestionText = week2QuestionText,
-            week2AnswerA = week2AnswerA,
-            week2AnswerB = week2AnswerB,
-            week2AnswerC = week2AnswerC,
-            week2AnswerD = week2AnswerD,
-            week2CorrectAnswer = week2CorrectAnswer
-        ).save()
-
-        Question2M(
-            rocket = rocket,
-            month2QuestionName = month2QuestionName,
-            month2ReviewText = month2ReviewText,
-            month2QuestionText = month2QuestionText,
-            month2AnswerA = month2AnswerA,
-            month2AnswerB = month2AnswerB,
-            month2AnswerC = month2AnswerC,
-            month2AnswerD = month2AnswerD,
-            month2CorrectAnswer = month2CorrectAnswer
-        ).save()
-
-        question2d = Question2D.objects.get(day2QuestionName = day2QuestionName )
-        question2w = Question2W.objects.get(week2QuestionName = week2QuestionName )
-        question2m = Question2M.objects.get(month2QuestionName = month2QuestionName )
-
-        Rocket.objects.filter(rocketName = rocketName).update(
-            question2d = question2d, 
-            question2w = question2w, 
-            question2m = question2m, 
+        rocketCheck = Rocket.objects.filter(rocketName = rocketName)
+        print(f'words?{rocketCheck}')
+        if (rocketCheck):
+            return JsonResponse({ 
+                'error': 'Rocket field Must be Unique'
+                },
+                safe = True,
+                status = status.HTTP_500_INTERNAL_SERVER_ERROR
         )
+        else:
+            print(f'before Try')
+            try:
+                print(f'after try called')
+                Rocket(
+                    rocketName = rocketName, 
+                    classKey = classKey, 
+                    user = username, 
+                ).save()
+                rocket = Rocket.objects.get(rocketName = rocketName)
+                Question2D(
+                    rocket = rocket,
+                    day2QuestionName = day2QuestionName,
+                    day2ReviewText = day2ReviewText,
+                    day2QuestionText = day2QuestionText,
+                    day2AnswerA = day2AnswerA,
+                    day2AnswerB = day2AnswerB,
+                    day2AnswerC = day2AnswerC,
+                    day2AnswerD = day2AnswerD,
+                    day2CorrectAnswer = day2CorrectAnswer
+                ).save()
+                Question2W(
+                    rocket = rocket,
+                    week2QuestionName = week2QuestionName,
+                    week2ReviewText = week2ReviewText,
+                    week2QuestionText = week2QuestionText,
+                    week2AnswerA = week2AnswerA,
+                    week2AnswerB = week2AnswerB,
+                    week2AnswerC = week2AnswerC,
+                    week2AnswerD = week2AnswerD,
+                    week2CorrectAnswer = week2CorrectAnswer
+                ).save()
+                Question2M(
+                    rocket = rocket,
+                    month2QuestionName = month2QuestionName,
+                    month2ReviewText = month2ReviewText,
+                    month2QuestionText = month2QuestionText,
+                    month2AnswerA = month2AnswerA,
+                    month2AnswerB = month2AnswerB,
+                    month2AnswerC = month2AnswerC,
+                    month2AnswerD = month2AnswerD,
+                    month2CorrectAnswer = month2CorrectAnswer
+                ).save()
+                question2d = Question2D.objects.get(day2QuestionName = day2QuestionName )
+                question2w = Question2W.objects.get(week2QuestionName = week2QuestionName )
+                question2m = Question2M.objects.get(month2QuestionName = month2QuestionName )
+                Rocket.objects.filter(rocketName = rocketName).update(
+                    question2d = question2d, 
+                    question2w = question2w, 
+                    question2m = question2m, 
+                )
+                response = JsonResponse({
+                        'msg': 'successful'
+                    },
+                    safe=True,
+                    status=status.HTTP_201_CREATED
+                )                
+                print(f'afterResponse')
 
-        response = JsonResponse({
-                'msg': 'successful'
-            },
-            safe=True,
-            status=status.HTTP_201_CREATED
-        )
-        return response
+
+            except IntegrityError:
+                Rocket.objects.filter(rocketName = rocketName).delete()
+                print(f'Integrity Error')
+                return JsonResponse({ 
+                    'error': 'All Fields Must be Unique'
+                    },
+                    safe = True,
+                    status = status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+            return response
 
 class SubscriptionSerializer(serializers.Serializer):
     source = serializers.CharField(max_length=256)
