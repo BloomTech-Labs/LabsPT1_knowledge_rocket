348e05512cec32e534e1081a4b02d9853fab584b
Bhavik Ravani <ravanibhavik@gmail.com>
2019-01-26 23:12:43 -0500

    Added handle methods to handle user input and updating students list.

--- frontend/src/components/createClass.js
+++ frontend/src/components/createClass.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Button, Form, Label, Input, FormGroup, Container, Row, Col, Card, Badge, CardTitle, CardText } from "reactstrap";
+import { Button, Form, Label, CardBody, Input, FormGroup, Container, Row, Col, Card, Badge, CardTitle, CardText } from "reactstrap";
 
 import SidebarNav from "./SidebarNav";
 
-import "../css/SidebarNav.css";
+import "../css/CreateClass.css";
 
 const dummyRockets = [
     { "rocket_name": "CSPT1",}, 
@@ -22,18 +22,37 @@ class CreateClass extends Component {
         rockets: []
     }
 
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleAddStudent = (e) => {
+        e.preventDefault();
+        const students = this.state.students;
+        // let id = students.length;
+        // console.log(this.state.studentFirstName);
+        students.push({
+                       lastName: this.state.studentLastName,
+                       firstName: this.state.studentFirstName,
+                       email: this.state.studentEmail
+                    })
+        // console.log(students);
+        this.setState({ students, studentFirstName: "", studentLastName: "", studentEmail:"" })
+    }
+
     render() {
         return (
-            <Container>
+            // <Container>
                 <Row>
-                    <Col sm="md" lg="3">
+                    <Col lg="3">
                         <SidebarNav />
                     </Col>
                     <Col lg="9">
                         <Row>
-                            <Form style={{ width: 200}}>
+                            <Form>
                                 <FormGroup>
-                                <h3>Settings</h3>
+                                    <h3>Settings</h3>
                                     <Row>
                                         <Col>
                                             <Input
@@ -42,20 +61,20 @@ class CreateClass extends Component {
                                             id="name" 
                                             maxLength="50"
                                             placeholder="Class Name"
-                                            value=""
-                                            // onChange={}
+                                            value={this.state.className}
+                                            onChange={this.handleChange}
                                             />
                                         </Col>
+                                        {' '}
                                         <Col>
-                                            <Input type="checkbox"
-                                            />
+                                            <Input type="checkbox" />
                                         </Col>
                                     </Row>
                                 </FormGroup>
                             </Form>
-                            <Form>
-                            <FormGroup>
-                            <h3>Add Students</h3>
+                            <Form onSubmit={this.handleAddStudent}>
+                                <FormGroup>
+                                    <h3>Add Students</h3>
                                     <Row>
                                         <Col>
                                             <Input
@@ -64,19 +83,19 @@ class CreateClass extends Component {
                                                 id="lastname" 
                                                 maxLength="50"
                                                 placeholder="Last Name"
-                                                value=""
-                                                // onChange={}
+                                                value={this.state.studentLastName}
+                                                onChange={this.handleChange}
                                                 />
                                         </Col>
                                         <Col>
                                             <Input
                                                 type="text" 
-                                                name="studenFirsttName" 
+                                                name="studentFirstName" 
                                                 id="firstname" 
                                                 maxLength="50"
                                                 placeholder="First Name"
-                                                value=""
-                                                // onChange={}
+                                                value={this.state.studentFirstName}
+                                                onChange={this.handleChange}
                                                 />
                                         </Col>
                                         <Col>
@@ -86,8 +105,8 @@ class CreateClass extends Component {
                                                 id="email" 
                                                 maxLength="50"
                                                 placeholder="Email"
-                                                value=""
-                                                // onChange={}
+                                                value={this.state.studentEmail}
+                                                onChange={this.handleChange}
                                                 />
                                         </Col>
                                         <Col>
@@ -96,20 +115,45 @@ class CreateClass extends Component {
                                     </Row>
                                 </FormGroup>
                             </Form>
-                        </Row>
-                        <h3>Knwoledge Rockets</h3>
-                        <Row>
-                            {dummyRockets.map((unit) => (
-                            <Col md="4" sm="6" xs="12" className="mb-4">
-                                <Card body>
-                                    <CardTitle className="text-center">{unit.rocket_name}</CardTitle>
-                                </Card>
-                            </Col>
-                            ))}
+                            <Form>
+                                <h3>Students</h3>
+                                <Row>
+                                    {this.state.students.map((student, id) => {                                
+                                        // console.log(student);  
+                                        return (
+                                            <Col md="4" sm="6" xs="12" className="mb-4" key={id}>
+                                        <Card>
+                                            <CardBody>
+                                                <CardTitle className="student-heading">
+                                                    <Button close />
+                                                </CardTitle>
+                                                <CardText className="text-center">
+                                                    {student.lastName} {' '} {student.firstName}
+                                                </CardText>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                    )})}
+                                </Row>
+                            </Form>
+                            <Form>
+                                <h3>Knowledge Rockets</h3>
+                                <Row>
+                                    {dummyRockets.map((unit, id) => (
+                                        <Col md="4" sm="6" xs="12" className="mb-4" key={id}>
+                                        <Card>
+                                            <CardBody>
+                                                <CardTitle className="text-center">{unit.rocket_name}</CardTitle>
+                                            </CardBody>
+                                        </Card>
+                                    </Col>
+                                    ))}
+                                </Row>
+                            </Form>
                         </Row>
                     </Col>
                 </Row>
-            </Container>
+            // </Container>
         )
     }
 
