0214358526c0358f97d9e2151c675ea7528a8d8e
unknown <jesse.reichel@live.com>
2019-02-07 17:32:07 -0800

    fixed reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,88 +1,107 @@
-import {
-  LOADING,
-  REGISTER,
-  LOGIN,
-  CHANGE_LOADING,
-  ERROR,
-  CLEAR_ERROR,
-  GET_USER,
-  CLEAR_REDIRECT,
-  REDIRECT,
-  CLEAR_STATE,
-  CREATE_ROCKET,
-  GET_CLASSES,
-  GET_ROCKETS,
-  UPDATE_PASSWORD,
-  UPDATE_USER,
-  GET_QUIZ
-} from "../actions";
+import { 
+    LOADING, 
+    REGISTER, 
+    LOGIN, 
+    CHANGE_LOADING, 
+    ERROR, 
+    CLEAR_ERROR, 
+    GET_USER, 
+    CLEAR_REDIRECT, 
+    REDIRECT, 
+    CLEAR_STATE,
+    CREATE_ROCKET,
+    GET_ROCKETS,
+    GET_STUDENTS,
+    GET_CLASSES,
+    GET_ROCKETS_BY_CLASS,
+    UPDATE_PASSWORD,
+    UPDATE_USER,
+    GET_QUIZ,
+
+} from '../actions';
+
 
 // team discussion on what the state should look like
 
 const defaultState = {
-  userKey: "",
-  user: "",
-  loading: false,
-  success: false,
-  error: false,
-  errorMsg: "",
-  redirect: false,
-  rocket: "",
-  classes: "",
-  question: ""
-};
+    userKey: '',
+    user: '',
+    loading: false,
+    success: false,
+    error: false,
+    errorMsg: "",
+    redirect: false,
+    rockets: [],
+    students: [],
+    classes: [],
+    classRockets: [],
+    rocket: ''
+}
 
 export default (state = defaultState, action) => {
-  switch (action.type) {
-    case LOADING:
-      return { ...state, loading: true };
+    switch(action.type) {
+        case LOADING:
+            return {...state, loading: true }
 
-    case REGISTER:
-      return { ...state, userKey: [action.payload], success: true };
+        case REGISTER:
+            return {...state, userKey: [ action.payload ], success: true}
 
-    case LOGIN:
-      return { ...state, userKey: [action.payload], success: true };
+        case LOGIN:
+            return {...state, userKey: [ action.payload ], success: true}
 
-    case CHANGE_LOADING:
-      return { ...state, loading: false };
+        case CHANGE_LOADING:
+            return {...state, loading: false }
 
-    case GET_USER:
-      return { ...state, user: [action.payload], success: true };
+        case GET_USER:
+            return {...state, user: [action.payload], success: true }
+
+        // case GET_CLASSES:
+        //     return {...state, classes: [action.payload], success: true }
+
+        case GET_QUIZ:
+        return { ...state, question: action.payload, success: true };  
 
         case GET_ROCKETS:
             return { ...state, rockets: action.payload, success: true }
 
-    case GET_ROCKETS:
-      return { ...state, rockets: [action.payload], success: true };
+        case UPDATE_PASSWORD:
+            return {...state, user: [action.payload], success: true }
 
-    case GET_QUIZ:
-      return { ...state, question: action.payload, success: true };
+        case UPDATE_USER:
+            return {...state, user: [action.payload], success: true }
 
-    case UPDATE_PASSWORD:
-      return { ...state, user: [action.payload], success: true };
+        
+        case ERROR:
+            return {...state, error: true, errorMsg: action.payload}
+        
+        case CLEAR_ERROR:
+            return {...state, error: false, errorMsg: ''}
 
-    case UPDATE_USER:
-      return { ...state, user: [action.payload], success: true };
+        case REDIRECT:
+            return { ...state, redirect: true  }
 
-    case ERROR:
-      return { ...state, error: true, errorMsg: action.payload };
+        case CLEAR_REDIRECT:
+            return { ...state, redirect: false } 
 
-    case CLEAR_ERROR:
-      return { ...state, error: false, errorMsg: "" };
+        case CREATE_ROCKET:
+            return { ...state, rocket: action.payload }
+        
+        case CLEAR_STATE:
+            return {};
 
-    case REDIRECT:
-      return { ...state, redirect: true };
+        // case GET_ROCKETS:
+        //     return { ...state, rockets: action.payload }    
 
-    case CLEAR_REDIRECT:
-      return { ...state, redirect: false };
+        case GET_STUDENTS:
+            return { ...state, students: action.payload }
 
-    case CREATE_ROCKET:
-      return { ...state, rocket: action.payload };
+        case GET_CLASSES:
+            return { ...state, classes: action.payload }
 
-    case CLEAR_STATE:
-      return {};
+        case GET_ROCKETS_BY_CLASS:
+            return { ...state, classRockets: action.payload }
 
-    default:
-      return state;
-  }
-};
+        default:
+            return state;
+    }
+}
